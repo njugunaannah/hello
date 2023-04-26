160f3c5089bf7803190b1eb00a513ba2e4e4f95b
KollihTheGuruh <mmike8449@gmail.com>
2023-04-26 09:07:05 -0700

    shell

--- exit_shell.c
+++ exit_shell.c
@@ -46,74 +46,22 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	char *s = dest;
-
+	int j;
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		/* empty body, loop used to find end of dest string */
 	}
 
-	for (int j = 0; src[j] != '\0' && j < n; j++, i++)
+	for (j = 0; src[j] != '\0' && j < n; j++, i++)
 	{
 		dest[i] = src[j];
 	}
-	
-	int j;
 
 	if (j < n)
 	{
 		dest[i] = '\0';
 	}
 
-	return (s);
-}
-
-/**
- * _strchr - locate character in string
- * @s: the string to search
- * @c: the character to locate
- *
- * Return: pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found
- */
-
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
-
-	return (NULL);
-}
-
-/**
- * _strncat - Concatenates two strings, but with a specified
- *            maximum length for the source string.
- *
- * @dest: The destination string.
- * @src: The source string.
- * @n: The maximum number of bytes to concatenate from src.
- *
- * Return: A pointer to the concatenated string.
- */
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
 	return (s);
 }
 
